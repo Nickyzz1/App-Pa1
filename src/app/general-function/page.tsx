9513c66ae0d4b4de325f239c5aae5c3a9a435285
Nickyzz1 <nicolle.rodrigues.siqueira@gmail.com>
2024-10-03 16:41:41 -0300

    draggable

--- src/app/general-function/page.tsx
+++ src/app/general-function/page.tsx
@@ -1,6 +1,9 @@
 "use client"
 import { randomInt } from "crypto";
 import { useState } from "react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Draggable from 'react-draggable'; 
 
 interface HandleProps{
     a: string,
@@ -46,49 +49,45 @@ const GeneralFunction: React.FC = () =>
     const style = 
     {
         container: "flex flex-col h-screen justify-center items-center bg-gradient-to-r from-slate-950 to-slate-800 ",
-        box: "flex flex-col justify-center items-center bg-indigo-950 text-white w-auto h-auto p-6 rounded-lg shadow-[0_5px_20px_-2px_rgba(255,255,255,0.3)]",
+        box: "flex flex-col handle justify-center items-center bg-indigo-950 text-white w-auto h-auto p-6 rounded-lg shadow-[0_5px_20px_-2px_rgba(255,255,255,0.3)]",
         input: "rounded m-4 p-4 text-black",
         btn: "bg-indigo-900 m-4 p-4 rounded hover:bg-indigo-700 focus shadow-[0_5px_2px_2px_rgba(255,255,255,0.3)]"
     }
 
-    
-
-
     return(
         <>
             <div className={style.container} >
-                <div className={style.box}>
-                    <h1 className="text-2xl">Função reaproveitada</h1>
-                    <div>
-                        <input className={style.input} type="text" placeholder="numero 01"  value = {numero1} onChange={(e) => setNumero1(e.target.value)}/>
+            <Draggable>
+                    <div className={style.box}>
+                        <h1 className="text-2xl">Função reaproveitada</h1>
+                        <div>
+                            <input className={style.input} type="text" placeholder="numero 01"  value = {numero1} onChange={(e) => setNumero1(e.target.value)}/>
+                        </div>
+                        <div>
+                            <input className={style.input} type="text" placeholder="numero 02" value = {numero2} onChange={(e) => setNumero2(e.target.value)} />
+                        </div>
+                        <div>
+                            <button className={style.btn} onClick={() => handleAll({a:numero1, b: numero2})} >calcular todos</button>
+                        </div>
+                        <div>
+                            <h2>Resultado da Soma</h2>
+                            <p>{respSoma ? respSoma : erro}</p>
+                        </div>
+                        <div>
+                            <h2>Resultado da Subtração</h2>
+                            <p>{(numero1.trim() !== "" && numero2.trim() !== "" && !isNaN(respSub ?? NaN)) ? respSub : (numero1.trim() === "" || numero2.trim() === "" ? "" : erro)}</p>
+                            {/* {!isNaN( respSub?? NaN) ? respSub: erro } o ? verifica se o campo é um numero ou é undefined, se foir undefined retornsn Nan caso contario é verdadeiro */}
+                        </div>
+                        <div>
+                            <h2>Resultado da Multiplicação</h2>
+                            <p>{(numero1.trim() !== "" && numero2.trim() !== "" && !isNaN(respMult ?? NaN)) ? respMult : (numero1.trim() === "" || numero2.trim() === "" ? "" : erro)}</p>
+                        </div>
+                        <div>
+                            <h2>Resultado da Divisão</h2>
+                            <p>{(numero1.trim() !== "" && numero2.trim() !== "" && !isNaN(respDiv ?? NaN)) ? respDiv : (numero1.trim() === "" || numero2.trim() === "" ? "" : erro)}</p>
+                        </div>
                     </div>
-                    <div>
-                        <input className={style.input} type="text" placeholder="numero 02" value = {numero2} onChange={(e) => setNumero2(e.target.value)} />
-                    </div>
-                    <div>
-                        <button className={style.btn} onClick={() => handleAll({a:numero1, b: numero2})} >calcular todos</button>
-                    </div>
-                    <div>
-                        <h2>Resultado da Soma</h2>
-                        <p>{respSoma ? respSoma : erro}</p>
-
-                    </div>
-                    <div>
-                        <h2>Resultado da Subtração</h2>
-                        <p>{(numero1.trim() !== "" && numero2.trim() !== "" && !isNaN(respSub ?? NaN)) ? respSub : (numero1.trim() === "" || numero2.trim() === "" ? "" : erro)}</p>
-
-                        {/* {!isNaN( respSub?? NaN) ? respSub: erro } o ? verifica se o campo é um numero ou é undefined, se foir undefined retornsn Nan caso contario é verdadeiro */}
-                    </div>
-                    <div>
-                        <h2>Resultado da Multiplicação</h2>
-                        <p>{(numero1.trim() !== "" && numero2.trim() !== "" && !isNaN(respMult ?? NaN)) ? respMult : (numero1.trim() === "" || numero2.trim() === "" ? "" : erro)}</p>
-                    </div>
-                    <div>
-                        <h2>Resultado da Divisão</h2>
-                        <p>{(numero1.trim() !== "" && numero2.trim() !== "" && !isNaN(respDiv ?? NaN)) ? respDiv : (numero1.trim() === "" || numero2.trim() === "" ? "" : erro)}</p>
-
-                    </div>
-                </div>
+            </Draggable>
             </div>
 
         </>
